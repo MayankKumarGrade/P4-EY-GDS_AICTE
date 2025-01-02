7a8f7b0e41cf9268b081ee5cfce1d47c03ebe907
Mayank Kumar Grade <mayankkumargrade@gmail.com>
2025-01-03 01:00:31 +0530

    Add CartTotal component and integrate it into Cart page; enhance ShopContext with cart amount calculation and quantity update functionality

---
+++ frontend/src/components/CartTotal.jsx
@@ -0,0 +1,38 @@
+import React, {useContext} from 'react';
+import {ShopContext} from '../context/shopContext';
+import Title from './Title';
+
+const CartTotal = () => {
+
+    const{delivery_fee, currency, getCartAmount, navigate} = useContext(ShopContext);
+
+  return (
+    <div className='w-full border border-gray-300 rounded-lg shadow-lg p-6 hover:shadow-2xl hover:scale-105 transition-all duration-200 transform hover:translate-y-2'>
+      <div className="text-2xl">
+        <Title text1={'CART'} text2={'TOTAL'} />
+      </div>
+
+      <div className='flex flex-col gap-2 text-sm mt-2 mb-4'>
+        <div className='flex justify-between'>
+          <p>Subtotal</p>
+          <p>{currency}{getCartAmount()}.00</p>
+        </div>
+        <hr />
+        <div className='flex justify-between'>
+          <p>Delivery Charges</p>
+          <p>{currency}{delivery_fee}.00</p>
+        </div>
+        <hr />
+        <div className='flex justify-between'>
+          <b>Total</b>
+          <b>{currency}{getCartAmount() === 0 ? 0 : getCartAmount() + delivery_fee}.00</b>
+        </div>
+      </div>
+      <div>
+          <button onClick={()=>navigate('/place-order')} className='flex justify-center items-center bg-black text-white rounded-lg text-sm my-2 px-8 py-3'>MALE IT YOUR&apos;S</button>
+      </div>
+    </div>
+  )
+}
+
+export default CartTotal
--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -2,6 +2,7 @@ import React, { createContext, useState } from 'react';
 import { products } from '../assets/assets';
 import PropTypes from 'prop-types';
 import { toast } from 'react-toastify';
+import { useNavigate } from 'react-router-dom';
 
 export const ShopContext = createContext();
 
@@ -11,6 +12,7 @@ const ShopContextProvider = (props) => {
     const [search, setSearch] = useState('');
     const [showSearch, setShowSearch] = useState(false);
     const [cartItems, setCartItems] = useState({});
+    const navigate = useNavigate();
 
     const addToCart = async (itemId, size) => {
 
@@ -43,16 +45,42 @@ const ShopContextProvider = (props) => {
                 try {
                     if(cartItems[items][item] > 0) {
                     totalCount += cartItems[items][item];
-                } 
-            } catch (error) {
-                console.log(error);}
+                    } 
+                } catch (error) {
+                    console.log(error);}
+            }
         }
+        return totalCount;
     }
-    return totalCount;
-}
+
+    const updateQuantity = async (itemId, size, quantity) => {
+        let cartData = structuredClone(cartItems);
+        cartData[itemId][size] = quantity;
+
+        setCartItems(cartData);
+    }
+
+    const getCartAmount = () => {
+        let totalAmount = 0;
+        for (const items in cartItems) {
+            let itemInfo = products.find((product) => product._id === items);
+            for (const item in cartItems[items]) {
+                try {
+                    if(cartItems[items][item] > 0) {
+                        totalAmount += itemInfo.price * cartItems[items][item];
+                    }
+                } catch (error) {
+                    console.log(error);
+                }
+            }
+        }
+        return totalAmount;
+    }
 
     const value = {
-        products, currency, delivery_fee, search, setSearch, showSearch, setShowSearch, cartItems, addToCart, getCartCount,
+        products, currency, delivery_fee, search, setSearch, showSearch, setShowSearch, cartItems, addToCart, getCartCount, updateQuantity, getCartAmount, 
+        navigate
+
     };
 
     return (
