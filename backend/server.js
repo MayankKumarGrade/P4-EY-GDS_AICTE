0314c659afe17bf981f5b539af651d5416ba6951
Mayank Kumar Grade <mayankkumargrade@gmail.com>
2025-01-06 05:02:29 +0530

    Add cart functionality with authentication middleware and routes

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import connectCloudinary from "./config/cloudinary.js";
 import productModel from "./models/productModel.js";
 import userRouter from "./routes/userRoute.js";
 import productRouter from "./routes/productRoute.js";
+import cartRouter from "./routes/cartRoute.js";
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -17,6 +18,7 @@ app.use(cors());
 
 app.use("/api/user", userRouter);
 app.use("/api/product", productRouter);
+app.use("/api/cart", cartRouter)
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
