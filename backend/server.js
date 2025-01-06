2f6a41b691fd0ed64d3f89539e442ffbc106c80b
Mayank Kumar Grade <mayankkumargrade@gmail.com>
2025-01-06 22:08:32 +0530

    Refactor backend for Vercel deployment: create index.js as entry point and update vercel.json configuration

--- backend/server.js
+++ backend/server.js
@@ -3,27 +3,31 @@ import cors from "cors";
 import "dotenv/config";
 import connectDB from "./config/mongodb.js";
 import connectCloudinary from "./config/cloudinary.js";
-import productModel from "./models/productModel.js";
 import userRouter from "./routes/userRoute.js";
 import productRouter from "./routes/productRoute.js";
 import cartRouter from "./routes/cartRoute.js";
 import orderRouter from "./routes/orderRoute.js";
 
 const app = express();
+
+// Connect to database and cloudinary
 connectDB();
 connectCloudinary();
 
+// Middleware
 app.use(express.json());
 app.use(cors());
 
+// Routes
 app.use("/api/user", userRouter);
 app.use("/api/product", productRouter);
 app.use("/api/cart", cartRouter);
 app.use("/api/order", orderRouter);
 
+// Test Route
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-// Export the app for Vercel
+// Export for Vercel
 export default app;
